edcc495f802d45e69401438f4f3c3f868312955c
otonye77 <oamietubodie@gmail.com>
2026-03-14 13:23:09 +0100

    url fix

--- backend/user-service/src/main/java/org/arghyam/jalsoochak/user/controller/TenantStaffController.java
+++ backend/user-service/src/main/java/org/arghyam/jalsoochak/user/controller/TenantStaffController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/tenant")
+@RequestMapping("/api/v1/tenant/user")
 @RequiredArgsConstructor
 public class TenantStaffController {
 
